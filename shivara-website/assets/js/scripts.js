a8d8c153584daa854d61cbcb175e7ec5206232e4
Shivarta <alejandroaferi@gmail.com>
2025-03-06 00:26:37 +0000

    Create scripts.js

---
+++ shivara-website/assets/js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado con éxito!');
+    // Aquí puedes agregar la lógica para enviar el formulario a un servicio de backend o API
+});
